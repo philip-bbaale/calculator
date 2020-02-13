09764dfe71f4abdc04a3bed73026a0b64c6877cc
Pip Bbaale <namugera.philip@gmail.com>
2020-02-13 09:12:28 +0300

    adding subtraction division and multiplication

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,18 @@ return number1 + number2;
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 alert(number1+"+"+number2+"=" +add(number1,number2));
+
+var subtract = function (number1,number2){
+  return number1-number2;
+};
+alert(number1+"-"+number2+"=" +subtract(number1,number2));
+
+var multiply = function(number1,number2){
+  return number1*number2;
+};
+alert(number1+"*"+number2+"=" +multiply(number1,number2));
+
+var divide = function (number1,number2){
+  return number1/number2;
+};
+alert(number1+"/"+number2+"=" +divide(number1,number2));
