47916ae5351201372b283dd8d003b110c9f7d922
Pip Bbaale <namugera.philip@gmail.com>
2020-02-19 09:45:33 +0300

    adding subtraction js

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,21 @@ var add = function(number1, number2) {
  
  
  $(document).ready(function() {
+
   $("form#add").submit(function(event){
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
-    var result = add(number1,number2);
-    $("#output").text(result);
+    var resultadd = add(number1,number2);
+    $("#outputAdd").text(resultadd);
   });
+
+  $("form#subtract").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var resultsub = subtract(number1,number2);
+    $("#outputSub").text(resultsub);
+  });
+
  });
